4e6a6fabf005e8a61ca86f687a0a7df4ec530ab7
LuisMontenegro21 <83621697+LuisMontenegro21@users.noreply.github.com>
2022-02-06 09:07:13 -0600

    Create Sort.java

---
+++ Sort.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Sort{
+  
+  public void gnomeSort(){}
+  
+  public void mergeSort(){}
+  
+  public void quickSort(){}
+  
+  public void radixSort(){}
+  
+  public void otherSort(){}
+  
+}
